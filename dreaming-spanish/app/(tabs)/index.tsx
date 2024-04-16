67089bb4454e9e8bf84d448d226ffeff5160bfc2
DariusGuillaume <dariusguillaume2@gmail.com>
2024-04-16 09:08:05 -0400

    mainpage icon added

--- dreaming-spanish/app/(tabs)/index.tsx
+++ dreaming-spanish/app/(tabs)/index.tsx
@@ -1,19 +1,41 @@
-import { StyleSheet,Text,View } from "react-native";
+import { StyleSheet, Text, TouchableOpacity, View, Image } from "react-native";
 import React from "react";
+import { Stack } from "expo-router";
 
 const Page = () => {
-    return (
-        <View style={styles.container}>
-        <Text >Hello, World!</Text>
-        </View>
-    );
-    }
-    export default Page;
+  return (
+    <Stack.Screen
+      options={{
+        headerTransparent: true,
+        headerTitle: '',
+        headerLeft: () => (
+          <TouchableOpacity onPress={() => {}} style={styles.headerLeft}>
+            <Image
+              source={require('../../assets/images/home-logo.png')}
+              style={styles.logoImage}
+            />
+          </TouchableOpacity>
+        ),
+      }}
+    />
+  );
+};
 
-    const styles = StyleSheet.create({
-        container : { 
-            flex: 1,
-            justifyContent: 'center',
-            alignItems: 'center',
-            backgroundColor: '#F5FCFF',
-        } } ) ;
+export default Page;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  headerLeft: {
+    marginLeft: 20,
+  },
+  logoImage: {
+    width: 40,
+    height: 40,
+    borderRadius: 10,
+  },
+});
