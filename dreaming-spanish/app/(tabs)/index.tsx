254b9727ca55ec41f2af9cce942feeec75a2c3e3
DariusGuillaume <dariusguillaume2@gmail.com>
2024-04-16 10:10:04 -0400

    added goal bar

--- dreaming-spanish/app/(tabs)/index.tsx
+++ dreaming-spanish/app/(tabs)/index.tsx
@@ -3,18 +3,45 @@ import React from "react";
 import { Stack } from "expo-router";
 
 const Page = () => {
+  const minutesWatched = 20; // Replace with actual minutes watched
+  const maxDailyGoal = 60; // Replace with actual maximum daily goal
+
+  const progressPercentage = (minutesWatched / maxDailyGoal) * 100;
+
   return (
     <Stack.Screen
       options={{
         headerTransparent: true,
-        headerTitle: '',
+        headerTitle: "",
         headerLeft: () => (
           <TouchableOpacity onPress={() => {}} style={styles.headerLeft}>
             <Image
-              source={require('../../assets/images/home-logo.png')}
+              source={require("../../assets/images/home-logo.png")}
               style={styles.logoImage}
             />
           </TouchableOpacity>
+        ),
+        headerRight: () => (
+          <View style={styles.progressContainer}>
+            <View style={styles.progressTextContainer}>
+              <Text style={styles.progressText}>
+                Watched: <Text style={styles.boldText}>{minutesWatched} min</Text>
+              </Text>
+              <Text style={styles.progressText}>
+                Daily goal: <Text style={styles.boldText}>{maxDailyGoal} min</Text>
+              </Text>
+            </View>
+            <View style={styles.progressBarContainer}>
+              <View style={styles.progressBar}>
+                <View
+                  style={[
+                    styles.progressFill,
+                    { width: `${progressPercentage}%` },
+                  ]}
+                />
+              </View>
+            </View>
+          </View>
         ),
       }}
     />
@@ -26,16 +53,48 @@ export default Page;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#F5FCFF",
   },
   headerLeft: {
     marginLeft: 20,
+    marginTop: 10,
   },
   logoImage: {
     width: 40,
     height: 40,
     borderRadius: 10,
+  },
+  progressContainer: {
+    marginRight: 20,
+    marginTop: 10,
+  },
+  progressTextContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    marginBottom: 5,
+  },
+  progressText: {
+    fontSize: 12,
+    color: "#27283A",
+    marginRight: 10,
+  },
+  boldText: {
+    fontWeight: "bold",
+  },
+  progressBarContainer: {
+    alignItems: "center",
+  },
+  progressBar: {
+    width: 300,
+    height: 20,
+    backgroundColor: "#E0E0E0",
+    borderRadius: 10,
+    overflow: "hidden",
+  },
+  progressFill: {
+    height: "100%",
+    backgroundColor: "#FF6400",
   },
 });
